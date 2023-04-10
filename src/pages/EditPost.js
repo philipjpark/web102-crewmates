a8403f1c25cfec8b3626db830633e33dbb7b30ee
philipjpark <philipparknyc@gmail.com>
2023-04-10 01:22:38 -0400

    Delete Database Entires:

--- src/pages/EditPost.js
+++ src/pages/EditPost.js
@@ -1,29 +1,60 @@
-import React from 'react';
+import React, {useState, useEffect} from "react";
 import { useParams } from 'react-router-dom';
 import './EditPost.css'
+import { supabase } from "../client";
 
 const EditPost = ({data}) => {
 
     const {id} = useParams();
-    const post = data.filter(item => item.id === id)[0];
+    const [post, setPost] = useState({id: null, name: "", speed: "", color: ""});
+
+    useEffect(() => {
+        const result = data.filter(item => String(item.id) === id)[0];
+        setPost({title: result.title, author: result.author, description: result.description});
+    }, [data, id]);
+
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+        setPost( (prev) => {
+            return {
+                ...prev,
+                [name]:value,
+            }
+        });
+    }
+
+    const deletePost = async (event) => {
+        event.preventDefault();
+        const { error } = await supabase
+        .from("Posts")
+        .delete()
+        .eq("id", id);
+
+        if (error) {
+            console.log(error);
+            return;
+        }
+
+        window.location = "/";
+    }
 
     return (
         <div>
             <form>
-                <label for="title">Title</label> <br />
-                <input type="text" id="title" name="title" value={post.title} /><br />
+                <label htmlfor="name">Name</label> <br />
+                <input type="text" id="name" name="name" value={post.name} onChange={handleChange}/><br />
                 <br/>
 
-                <label for="author">Author</label><br />
-                <input type="text" id="author" name="author" value={post.author} /><br />
+                <label htmlfor="speed">Speed</label><br />
+                <input type="text" id="speed" name="speed" value={post.speed} onChange={handleChange}/><br />
                 <br/>
 
-                <label for="description">Description</label><br />
-                <textarea rows="5" cols="50" id="description" value={post.description} >
-                </textarea>
+                <label htmlfor="colors">Color</label><br />
+                <input type="text" id="color" name="color" onChange={handleChange}/><br />
                 <br/>
-                <input type="submit" value="Submit" />
-                <button className="deleteButton">Delete</button>
+
+                {/* <input type="submit" value="Submit" /> */}
+                <button className="deleteButton" onClick={deletePost}>Delete</button>
             </form>
         </div>
     )
